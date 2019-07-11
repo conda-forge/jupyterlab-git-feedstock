2dc34797ed8efaf36e265f38e0eb657cc930c91b
Daniel Bast <2790401+dbast@users.noreply.github.com>
2019-07-12 00:09:26 +0200

    Require jupyterlab <1.0.0, call entrypoints directly in link scripts

--- recipe/post-link.sh
+++ recipe/post-link.sh
@@ -1,1 +1,1 @@
-"${PREFIX}/bin/jupyter" serverextension enable jupyterlab_git --py --sys-prefix > "${PREFIX}/.messages.txt" 2>&1
+"${PREFIX}/bin/python" -m 'jupyter_core.command' serverextension enable jupyterlab_git --py --sys-prefix > "${PREFIX}/.messages.txt" 2>&1
