6fe8ee49b969d77df1c4bca737515e175d3b17cc
Frederic Collonval <frederic.collonval@safrangroup.com>
2019-09-05 02:07:58 -0400

    Build both as noarch and separate the frontend

--- recipe/install-lab.bat
+++ recipe/install-lab.bat
@@ -3,9 +3,6 @@
 CALL "%PREFIX%\Scripts\jupyter-labextension" install . --no-build || EXIT /B 1
 IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
 
-"%PYTHON%" -m pip install . --no-deps --ignore-installed -vvv || EXIT /B 1
-IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
-
 :: Shared file not to be included.
 del /Q "%PREFIX%\share\jupyter\lab\settings\build_config.json" || EXIT /B 1
 IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
--- recipe/install-lab.sh
+++ recipe/install-lab.sh
@@ -6,7 +6,5 @@ set -o xtrace -o nounset -o pipefail -o errexit
 # on jupyterlab startup, when the new extension is detected or was removed.
 "${PREFIX}/bin/jupyter" labextension install . --no-build
 
-"${PYTHON}" -m pip install . --no-deps --ignore-installed -vvv
-
 # Shared file not to be included.
 rm -f "${PREFIX}/share/jupyter/lab/settings/build_config.json"
