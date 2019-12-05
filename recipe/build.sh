e446c126812caf03c0edeadcf451a3243c6a4f62
Frédéric Collonval <fcollonval@gmail.com>
2019-12-05 09:29:39 +0100

    Use build script in meta.yaml

--- recipe/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -o xtrace -o nounset -o pipefail -o errexit
-
-"${PYTHON}" -m pip install . --no-deps --ignore-installed -vvv
-
-# Shared file not to be included.
-rm -f "${PREFIX}/share/jupyter/lab/settings/build_config.json"
