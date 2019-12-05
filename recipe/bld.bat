fa4cd0bbbe02b39df4a65127c1f605213e457fa4
Frédéric Collonval <fcollonval@gmail.com>
2019-12-05 09:29:52 +0100

    Use build script in meta.yaml

--- recipe/bld.bat
+++
@@ -1,6 +0,0 @@
-"%PYTHON%" -m pip install . --no-deps --ignore-installed -vvv || EXIT /B 1
-IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
-
-:: Shared file not to be included.
-del /Q "%PREFIX%\share\jupyter\lab\settings\build_config.json" || EXIT /B 1
-IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
